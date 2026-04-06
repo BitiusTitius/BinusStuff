3337513d2f562c2004179f99f3f2efe62a95dee5
BitiusTitius <122912878+BitiusTitius@users.noreply.github.com>
2026-04-06 21:33:49 +0700

    Add files via upload

---
+++ BinaryToDecimal.java
@@ -0,0 +1,31 @@
+public class BinaryToDecimal {
+    
+    public static String convertRecursion(int decimal) {
+        if (decimal == 0) {
+            return "";
+        }
+
+        return convertRecursion(decimal / 2) + (decimal % 2);
+    }
+
+    public static String convertIterative(int decimal) {
+        if (decimal == 0) {
+            return "0";
+        }
+
+        StringBuilder binary = new StringBuilder();
+
+        while (decimal > 0) {
+            int remainder = decimal % 2;
+            binary.insert(0, remainder);
+            decimal /= 2;
+        }
+
+        return binary.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertRecursion(67)); // Output: 1000011
+        System.out.println(convertIterative(67)); // Output: 1000011
+    }
+}
